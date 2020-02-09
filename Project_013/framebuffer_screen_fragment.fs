187f0c0c86b3e81f424bf985c1c7caa7d22181f9
Nie <yunie9447@qq.com>
2020-02-09 15:20:01 +0800

    minor change

--- Project_013/framebuffer_screen_fragment.fs
+++ Project_013/framebuffer_screen_fragment.fs
@@ -8,5 +8,6 @@ uniform sampler2D Texture;
 
 void main()
 {
-	FragColor = texture(Texture, TexCoord);
+	//FragColor = texture(Texture, TexCoord);
+	FragColor = vec4(vec3(1.0 - texture(Texture, TexCoord)), 1.0);
 }
